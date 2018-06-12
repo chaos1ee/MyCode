efec2532299693bc815ff0e35d4c7c7668727a14
maybeeee <lihao19900829@qq.com>
2018-06-12 14:24:56 +0800

    feat: refactor

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-一些小的demo。
+自己写的一些好玩的小demo。
 
-ball.html
-![ball.html](./Animation.gif)
+##### navigator.html
+导航条动画
+
+
+##### ball.html
+面向对象与JS动画
+
+![ball.html](image/Animation.gif)
+
+##### scale.html
+CSS动画
+
+![scale.html](image\scale.gif)
+
+##### pseudo.html
+CSS伪元素
+
+![scale.html](image\pseudo.png)
+
+##### validator.js
+Angular1表单校验类，策略模式。
+
+##### currying.js
+函数柯里化。
+
+##### subscribe.js
+订阅者模式
+
+##### sport.js
+运动框架
+
+##### negativeMargin.html
+负margin的应用
+
--- currying.js
+++ currying.js
@@ -1,7 +1,7 @@
 function currying(fn) {
   var slice = Array.prototype.slice,
-  args = slice.call(arguments, 1);
-  return function() {
+    args = slice.call(arguments, 1);
+  return function () {
     var innerArgs = slice.call(arguments);
     return fn.apply(null, args.concat(innerArgs));
   }
@@ -9,51 +9,52 @@ function currying(fn) {
 
 // demo1
 
-var Ajax = function() {
+var Ajax = function () {
   this.xhr = new XMLHttpRequest();
 }
 
-Ajax.prototype.open = function(type, url, params, callback) {
-  this.onload = function() {
+Ajax.prototype.open = function (type, url, params, callback) {
+  this.onload = function () {
     callback(JSON.parse(this.xhr.responseText), this.xhr.status, this.xhr);
   }
-  
+
   this.xhr.open(type, url, false);
   this.xhr.send(params);
 }
 
-['get', 'post', 'update', 'delete'].forEach(function(method){
+['get', 'post', 'update', 'delete'].forEach(function (method) {
   Ajax.prototype[method] = currying(Ajax.prototype.open, method);
 });
 
 
 var xhr = new Ajax();
 
-xhr.get('/music/123', {}, function(res) {
+xhr.get('/music/123', {}, function (res) {
   console.log(res);
 });
 
-xhr.post('/music/456', {}, function(res) {
+xhr.post('/music/456', {}, function (res) {
   console.log(res);
 });
 
 
 // demo2 延迟执行
 
-var add = function() {
+var add = function () {
   var that = this,
-  args = arguments;
-  return function() {
-    if(!aruguments.length) {
+    args = arguments;
+  return function () {
+    if (!aruguments.length) {
       var sum = 0;
-      for(var i = 0, c; c = args[i++];) {
+      for (var i = 0, c; c = args[i++];) {
         sum += c;
       }
       return sum;
     } else {
       Array.prototype.push.apply(args, arguments);
       return arguments.callee;
-  }  
+    }
+  }
 }
-  
-add(1)(2)(3)(4)(); // 10
+
+add(1)(2)(3)(4)();
